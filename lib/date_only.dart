2446bdda534effc5af9b1844cfc99a885dcb213f
ssk <developer.kss@gmail.com>
2022-05-25 11:09:16 +0900

    implement fromDateTime

--- lib/date_only.dart
+++ lib/date_only.dart
@@ -2,6 +2,22 @@ library date_only;
 
 /// DateOnly for present year / month / day only.
 class DateOnly {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+
+  /// default constructor
+  DateOnly({
+    required this.year,
+    required this.month,
+    required this.day,
+  });
+
+  final int year;
+  final int month;
+  final int day;
+
+  /// factory fromDateTime
+  factory DateOnly.fromDateTime(DateTime dateTime) => DateOnly(
+        year: dateTime.year,
+        month: dateTime.month,
+        day: dateTime.day,
+      );
 }
