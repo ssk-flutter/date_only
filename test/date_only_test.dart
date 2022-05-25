1c915e0b6381e699186448e2eb486f0b27c4a6bc
ssk <developer.kss@gmail.com>
2022-05-25 11:08:49 +0900

    test fromDateTime

--- test/date_only_test.dart
+++ test/date_only_test.dart
@@ -3,10 +3,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:date_only/date_only.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = DateOnly();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('fromDateTime', () {
+    final dateTime = DateTime(1980, 7, 26, 1, 1, 1, 1);
+    final dateOnly = DateOnly.fromDateTime(dateTime);
+    expect(dateOnly.year, dateTime.year);
+    expect(dateOnly.month, dateTime.month);
+    expect(dateOnly.day, dateTime.day);
   });
+
 }
