14d9c5efbcc7f0233db9a174c34f0632a3ba87ce
ssk <developer.kss@gmail.com>
2022-05-25 11:17:40 +0900

    implement now

--- lib/date_only.dart
+++ lib/date_only.dart
@@ -2,7 +2,6 @@ library date_only;
 
 /// DateOnly for present year / month / day only.
 class DateOnly {
-
   /// default constructor
   DateOnly({
     required this.year,
@@ -20,4 +19,21 @@ class DateOnly {
         month: dateTime.month,
         day: dateTime.day,
       );
+
+  /// factory now
+  factory DateOnly.now() => DateOnly.fromDateTime(DateTime.now());
+
+  @override
+  String toString() {
+    String _addLeadingZeroIfNeeded(int value) {
+      if (value < 10) return '0$value';
+      return value.toString();
+    }
+
+    final String yearLabel = _addLeadingZeroIfNeeded(year);
+    final String monthLabel = _addLeadingZeroIfNeeded(month);
+    final String dayLabel = _addLeadingZeroIfNeeded(day);
+
+    return '$DateOnly($yearLabel-$monthLabel-$dayLabel)';
+  }
 }
